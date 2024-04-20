4bdf498bb84645286db8c0bd93606b90ade94cd9
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-04-20 21:53:28 +0300

    Update app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,81 @@
-function App() {
-    return(
-        <div className="app">
+import { useState } from "react";
 
+function App() {
+
+    const [data, setData] = useState({});
+    const [town, setTown] = useState('');
+
+    const key = 'c4e1a2aec8a82a69db044c4f80690f5e';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${town}&units=metric&appid=${key}`;
+
+    const searchWeather = (event) => {
+        if (event.key === 'Enter') {
+            fetch(url)
+                .then((response) => response.json())
+                .then((response) => {
+                    setData(response);
+                })
+                setTown('');
+        }
+    }
+
+    return (
+      <div className="app">
+        <div className="inp-field">
+          <input
+            type="text"
+            value={town}
+            onChange={event => setTown(event.target.value)}
+            placeholder="Enter location"
+            onKeyDown={searchWeather}
+          />
         </div>
-    )
+        <div className="container">
+          <div className="header">
+            <div className="city">
+              <p>{data.name}</p>
+            </div>
+          </div>
+          <div className="temp">
+            {data.main ? (
+              <h1>
+                {data.main.temp.toFixed()}
+                °C
+              </h1>
+            ) : null}
+          </div>
+          <div className="desc">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+        {data.name !== undefined && (
+          <div className="footer">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">
+                  {data.main.feels_like}
+                  °C
+                </p>
+              ) : null}
+              <p>Відчувається як</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+              <p>Вологість</p>
+            </div>
+            <div className="wind">
+                {data.wind ? (
+                    <p className="bold">
+                        {`${data.wind.speed} `}
+                        M/C
+                    </p>
+                ) : null}
+                <p>Вітер</p>
+            </div>        
+          </div>
+        )}
+      </div>
+    );
 };
 
 export default App;
